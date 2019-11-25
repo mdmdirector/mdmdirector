1a8b911970ddb270eb76007322ba266e929cc4a8
Graham Gilbert <graham.gilbert@airbnb.com>
2019-11-25 10:13:27 -0800

    Stash

--- types/background_tasks.go
+++ types/background_tasks.go
@@ -1,5 +1,7 @@
 package types
 
+import uuid "github.com/satori/go.uuid"
+
 type DeviceFromMDM struct {
 	SerialNumber     string `json:"serial_number"`
 	UDID             string `json:"udid"`
@@ -9,4 +11,10 @@ type DeviceFromMDM struct {
 
 type DevicesFromMDM struct {
 	Devices []DeviceFromMDM `json:"devices"`
+}
+
+type ScheduledPush struct {
+	ID         uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
+	DeviceUDID string
+	Status     string `gorm:"default:'pending'"`
 }
