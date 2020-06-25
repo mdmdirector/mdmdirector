b22457f47c4b2c082e20063d5be5941e83fb4200
Graham Gilbert <graham.gilbert@airbnb.com>
2020-06-24 18:01:18 -0700

    More metrics

--- prometheus/prometheus.go
+++ prometheus/prometheus.go
@@ -10,7 +10,11 @@ import (
 )
 
 func Metrics() {
+	totalDevices()
+	profiles()
+}
 
+func totalDevices() {
 	var devices []types.Device
 	var count float64
 	totalDevices := prometheus.NewGauge(prometheus.GaugeOpts{
@@ -29,6 +33,50 @@ func Metrics() {
 				log.Error(err)
 			}
 			totalDevices.Set(count)
+		}
+	}()
+}
+
+func profiles() {
+	var sharedprofiles []types.SharedProfile
+	var count float64
+	totalSharedProfiles := prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "micromdm",
+		Subsystem: "profiles",
+		Name:      "sharedprofilescount",
+		Help:      "Total number of shared profiles in MDMDirector",
+	})
+	// register totalSharedProfiles
+	prometheus.MustRegister(totalSharedProfiles)
+	// loop over the ticker and update the total devices every 10 seconds
+	go func() {
+		for range time.Tick(time.Second * 10) {
+			err := db.DB.Find(&sharedprofiles).Count(&count).Error
+			if err != nil {
+				log.Error(err)
+			}
+			totalSharedProfiles.Set(count)
+		}
+	}()
+
+	var deviceprofiles []types.DeviceProfile
+	var deviceprofilescount float64
+	totalDeviceProfiles := prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "micromdm",
+		Subsystem: "profiles",
+		Name:      "deviceprofilescount",
+		Help:      "Total number of device profiles in MDMDirector",
+	})
+	// register totalDeviceProfiles
+	prometheus.MustRegister(totalDeviceProfiles)
+	// loop over the ticker and update the total devices every 10 seconds
+	go func() {
+		for range time.Tick(time.Second * 10) {
+			err := db.DB.Find(&deviceprofiles).Count(&deviceprofilescount).Error
+			if err != nil {
+				log.Error(err)
+			}
+			totalDeviceProfiles.Set(deviceprofilescount)
 		}
 	}()
 }
