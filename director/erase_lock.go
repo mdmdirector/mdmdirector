4555aaea73d939fc18f1fbb7768912b05dab842a
Graham Gilbert <graham.gilbert@airbnb.com>
2020-09-29 16:27:53 -0700

    Just return if no escrow url is set

--- director/erase_lock.go
+++ director/erase_lock.go
@@ -66,7 +66,11 @@ func EraseLockDevice(udid string) error {
 }
 
 func escrowPin(device types.Device, pin string) error {
-	endpoint, err := url.Parse(utils.EscrowURL())
+	escrowURL := utils.EscrowURL()
+	if escrowURL == "" {
+		return nil
+	}
+	endpoint, err := url.Parse(escrowURL)
 	if err != nil {
 		return errors.Wrap(err, "escrowPin:URL")
 	}
