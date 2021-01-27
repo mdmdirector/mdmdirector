a5a2178cbf47597e2ee023690c937d8fc74477f2
Graham Gilbert <graham.gilbert@airbnb.com>
2021-01-26 19:06:12 -0800

    Fix saving Security info

--- director/security_info.go
+++ director/security_info.go
@@ -3,8 +3,10 @@ package director
 import (
 	"github.com/mdmdirector/mdmdirector/db"
 	"github.com/mdmdirector/mdmdirector/types"
+	"github.com/mdmdirector/mdmdirector/utils"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
+	"gorm.io/gorm"
 )
 
 func RequestSecurityInfo(device types.Device) error {
@@ -22,40 +24,54 @@ func RequestSecurityInfo(device types.Device) error {
 }
 
 func SaveSecurityInfo(securityInfoData types.SecurityInfoData, device types.Device) error {
-	var securityInfo types.SecurityInfo
-	var managementStatus types.ManagementStatus
-	var firmwarePasswordStatus types.FirmwarePasswordStatus
-	var firewallSettings types.FirewallSettings
-	// var firewallSettingsApplications []types.FirewallSettingsApplication
-	var secureBoot types.SecureBoot
-	var secureBootReducedSecurity types.SecureBootReducedSecurity
-	securityInfo = securityInfoData.SecurityInfo
-	managementStatus = securityInfo.ManagementStatus
-	firmwarePasswordStatus = securityInfo.FirmwarePasswordStatus
-	firewallSettings = securityInfo.FirewallSettings
-	// firewallSettingsApplications = firewallSettings.FirewallSettingsApplications
-	secureBoot = securityInfo.SecureBoot
+	// var securityInfo types.SecurityInfo
+	// var managementStatus types.ManagementStatus
+	// var firmwarePasswordStatus types.FirmwarePasswordStatus
+	// var firewallSettings types.FirewallSettings
+	// // var firewallSettingsApplications []types.FirewallSettingsApplication
+	// var secureBoot types.SecureBoot
+	// var secureBootReducedSecurity types.SecureBootReducedSecurity
+	// securityInfo.DeviceUDID = device.UDID
+	// securityInfo = securityInfoData.SecurityInfo
+	// managementStatus = securityInfo.ManagementStatus
+	// managementStatus.DeviceUDID = device.UDID
+	// firmwarePasswordStatus = securityInfo.FirmwarePasswordStatus
+	// firmwarePasswordStatus.DeviceUDID = device.UDID
+	// firewallSettings = securityInfo.FirewallSettings
+	// firewallSettings.DeviceUDID = device.UDID
+	// // firewallSettingsApplications = firewallSettings.FirewallSettingsApplications
+	// secureBoot = securityInfo.SecureBoot
 	// secureBoot.DeviceUDID = device.UDID
-	secureBootReducedSecurity = securityInfo.SecureBoot.SecureBootReducedSecurity
-	secureBootReducedSecurity.DeviceUDID = device.UDID
+	// secureBootReducedSecurity = securityInfo.SecureBoot.SecureBootReducedSecurity
+	// secureBootReducedSecurity.DeviceUDID = device.UDID
+	// log.Info(device)
+
+	securityInfo := securityInfoData.SecurityInfo
 	securityInfo.DeviceUDID = device.UDID
+	securityInfo.FirewallSettings.DeviceUDID = device.UDID
+	securityInfo.FirmwarePasswordStatus.DeviceUDID = device.UDID
+	securityInfo.ManagementStatus.DeviceUDID = device.UDID
+	securityInfo.SecureBoot.DeviceUDID = device.UDID
+	securityInfo.SecureBoot.SecureBootReducedSecurity.DeviceUDID = device.UDID
+	utils.PrintStruct(securityInfo)
+
 	InfoLogger(LogHolder{DeviceUDID: device.UDID, DeviceSerial: device.SerialNumber, Message: "Saving SecurityInfo"})
-	err := db.DB.Model(&device).Association("SecurityInfo").Replace(&securityInfo)
+	err := db.DB.Session(&gorm.Session{FullSaveAssociations: true}).Model(&securityInfo).Updates(&securityInfo).Error
 	if err != nil {
 		return errors.Wrap(err, "Replace SecurityInfo Association")
 	}
 
-	err = db.DB.Model(&securityInfo).Association("FirmwarePasswordStatus").Append(&firmwarePasswordStatus)
+	err = db.DB.Session(&gorm.Session{FullSaveAssociations: true}).Model(&securityInfo.FirmwarePasswordStatus).Updates(&securityInfo.FirmwarePasswordStatus).Error
 	if err != nil {
 		return errors.Wrap(err, "Append FirmwarePasswordStatus Association")
 	}
 
-	err = db.DB.Model(&securityInfo).Association("ManagementStatus").Append(&managementStatus)
+	err = db.DB.Session(&gorm.Session{FullSaveAssociations: true}).Model(&securityInfo.ManagementStatus).Updates(&securityInfo.ManagementStatus).Error
 	if err != nil {
 		return errors.Wrap(err, "Append ManagementStatus Association")
 	}
 
-	err = db.DB.Model(&securityInfo).Association("FirewallSettings").Append(&firewallSettings)
+	err = db.DB.Session(&gorm.Session{FullSaveAssociations: true}).Model(&securityInfo.FirewallSettings).Updates(&securityInfo.FirewallSettings).Error
 	if err != nil {
 		return errors.Wrap(err, "Append FirewallSettings Association")
 	}
@@ -65,12 +81,12 @@ func SaveSecurityInfo(securityInfoData types.SecurityInfoData, device types.Devi
 	// 	ErrorLogger(LogHolder{Message: err.Error()})
 	// }
 
-	err = db.DB.Model(&securityInfo).Association("SecureBoot").Append(&secureBoot)
+	err = db.DB.Session(&gorm.Session{FullSaveAssociations: true}).Model(&securityInfo.SecureBoot).Updates(&securityInfo.SecureBoot).Error
 	if err != nil {
 		return errors.Wrap(err, "Append SecureBoot Association")
 	}
 
-	err = db.DB.Model(&secureBoot).Association("SecureBootReducedSecurity").Append(&secureBootReducedSecurity)
+	err = db.DB.Session(&gorm.Session{FullSaveAssociations: true}).Model(&securityInfo.SecureBoot.SecureBootReducedSecurity).Updates(&securityInfo.SecureBoot.SecureBootReducedSecurity).Error
 	if err != nil {
 		return errors.Wrap(err, "Append SecureBootReducedSecurity Association")
 	}
