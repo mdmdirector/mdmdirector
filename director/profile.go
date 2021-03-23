504ab2e202578bac4fb4a91cf547efafc3f9c6e9
Graham Gilbert <graham.gilbert@airbnb.com>
2021-03-23 11:55:13 -0700

    Make the linter happy

--- director/profile.go
+++ director/profile.go
@@ -705,8 +705,9 @@ func VerifyMDMProfiles(profileListData types.ProfileListData, device types.Devic
 	}
 
 	// For each, loop over the present profiles
-	for _, savedProfile := range profiles {
+	for profileI := range profiles {
 		found := false
+		savedProfile := profiles[profileI]
 		for i := range profileListData.ProfileList {
 			incomingProfile := profileListData.ProfileList[i]
 			if savedProfile.HashedPayloadUUID == incomingProfile.PayloadUUID && savedProfile.PayloadIdentifier == incomingProfile.PayloadIdentifier {
